00f4da144c2cb9584a1b451144fb2a1f9cc0f35b
Łukasz Kowol <lkowol@MacBook-Pro-ukasz.local>
2021-04-24 10:06:05 +0200

    Initial

---
+++ chessableBanking.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+dockerBuild () {
+   cd docker && docker-compose build && cd ..
+}
+
+run () {
+   cd docker && docker-compose up &
+   sleep 30
+   mutagen project terminate
+   mutagen project start
+}
+
+stop () {
+   mutagen project terminate
+   cd docker && docker-compose stop
+   mutagen terminate --all
+}
+
+reloadServices () {
+  docker exec docker_php-fpm_1 supervisorctl reload
+  echo "Services reloaded"
+}
+
+case $1 in
+  build)
+    dockerBuild
+  ;;
+  run)
+    run
+  ;;
+  stop)
+    stop
+  ;;
+  reload-services)
+      reloadServices
+  ;;
+esac
